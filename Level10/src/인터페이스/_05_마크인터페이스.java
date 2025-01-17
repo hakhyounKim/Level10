eb14febf2dec070cef243812dc3d22daa5585f13
hakhyounKim <hakhyoun721@gmail.com>
2025-01-17 10:04:54 +0900

    Create _05_마크인터페이스.java

---
+++ Level10/src/인터페이스/_05_마크인터페이스.java
@@ -0,0 +1,16 @@
+package 인터페이스;
+
+
+
+
+public class _05_마크인터페이스 {
+
+	public static void main(String[] args) {
+		
+		Tank tank = new Tank();
+		Dropship dropShip = new Dropship();
+		
+		
+	}
+
+}
