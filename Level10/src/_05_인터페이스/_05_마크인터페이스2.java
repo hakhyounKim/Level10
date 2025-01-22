b33f9420aca85bb9b2394b84e3d9fb518b093f02
hakhyounKim <hakhyoun721@gmail.com>
2025-01-22 13:22:42 +0900

    feat_khh0122

---
+++ Level10/src/_05_인터페이스/_05_마크인터페이스2.java
@@ -0,0 +1,23 @@
+package _05_인터페이스;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+//damage() 메소드를 가진 
+//Interface Damageable 를 정의하고
+//최하위 유닛들 (모든) 에게 Damageable 상속 
+//main에서 각각의 유닛들을 손상시킨 후 -> repair() 돌려보기
+
+
+//마크 인터페이스 : 안에 구현부는 없고 클래스를 구분하기 위해서 사용하는 인터페이스 
+
+
+
+public class _05_마크인터페이스2 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+}
---
+++ Level10/src/_13_다형성가이드/GameManager.java
@@ -0,0 +1,8 @@
+package _13_다형성가이드;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameManager {
+
+}
